9b0ab05ec6cb7a95f42fd890ecdfe20d6960306c
Nhat Minh Nguyen <minhnhat93@gmail.com>
2015-06-23 16:20:47 -0700

    lcod_bprop finished

--- code/lcod_bprop.m
+++ code/lcod_bprop.m
@@ -1,10 +1,16 @@
-function [ output_args ] = lcod_bprop( Zstar, X, Z, We, S, theta, deltaX, deltaWe, deltaS, deltatheta, e, k, b, B )
+function [ dWe, dS, dtheta, dX ] = lcod_bprop( Zstar, X, Z, We, S, theta, e, k, b, B )
 %LCOD_BPROP Summary of this function goes here
-%  e,k,b,B was saved in fprop
-  deltaS=0;
+%  e,k,b,B result of lcod_fprop
+  dS=0;
   deltaZ=0;
-  deltaB=
-
-
+  deltaB=h_prime(B,theta)*(Z-Zstar);
+  for t=T-1:-1:1
+    k1=k(t); de=sum(deltaB.*S(:,k1));
+    dS(:,k1)=dS(:,k1)+deltaB*e(t);
+    deltaB(k1)=deltaB(k1)+h_prime(b(t),theta(k1))*(deltaZ(k1)+de);
+    dtheta(k1)=dtheta(k1)-sign(b(t))*h_prime(b(t),theta(k1))*(deltaZ(k1)+de);
+    deltaZ(k)=-de;
+  end
+  dWe=deltaB*X';
+  dX=We'*deltaB;
 end
-
---
+++ code/train_lcod.m
@@ -0,0 +1,7 @@
+function [ output_args ] = train( input_args )
+%TRAIN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
