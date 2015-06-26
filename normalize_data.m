965d17e0c82366883f2acb60ca3945a6284cc9c7
Nhat Minh Nguyen <minhnhat93@gmail.com>
2015-06-26 15:23:01 -0700

    1

--- normalize_data.m
+++ normalize_data.m
@@ -1,11 +1,26 @@
-load('USPS Data/Dictionary2.mat');
+% load('USPS Data/Dictionary2.mat');
+% datapath='USPS data/';
+% train_data=load([datapath 'USPS_Train_Data.mat']);
+% train_data=train_data.Train_Data;
+% Wd=Dict;
+% coeff_trainset=zeros(size(Dict,2),size(train_data,2));
+% [Wd,w]=col_norm(Wd,2);
+% for j=1:500%size(train_data,2)
+%   disp(j);
+%   coeff_trainset(:,j)=cod(train_data(:,j),Wd, 0.0001, 0.0001);
+% end
+% save('normalized_data/dict_2000.mat','Wd');
+% save('normalized_data/coeff_train_2000.mat','coeff_trainset');
+load('USPS Data/Dictionary1.mat');
 datapath='USPS data/';
 train_data=load([datapath 'USPS_Train_Data.mat']);
 train_data=train_data.Train_Data;
 Wd=Dict;
 coeff_trainset=zeros(size(Dict,2),size(train_data,2));
 [Wd,w]=col_norm(Wd,2);
-for j=1:size(train_data,2)
+for j=1:500%size(train_data,2)
   disp(j);
   coeff_trainset(:,j)=cod(train_data(:,j),Wd, 0.0001, 0.0001);
 end
+save('normalized_data/dict_1000.mat','Wd');
+save('normalized_data/coeff_train_1000.mat','coeff_trainset');
